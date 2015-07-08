8c68358612c7ec0d8b721c446134946a8950e0a9
izumin5210 <masayuki@izumin.info>
2015-07-08 14:36:54 +0900

    Add DI annotations to routes.js

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -3,13 +3,20 @@ import {appName, templateFileExt, rootTemplateName} from "./constants";
 
 const app = angular.module(appName);
 
-app.config(($routeProvider, $locationProvider) => {
+let routes = ($routeProvider, $locationProvider) => {
   $locationProvider.html5Mode(false)
   $routeProvider
-    .when("/", {
-      templateUrl: `${rootTemplateName}.${templateFileExt}`
-    })
-    .when("/:page", {
-      templateUrl: (params) => `${params.page}.${templateFileExt}`
-    });
-});
+  .when("/", {
+    templateUrl: `${rootTemplateName}.${templateFileExt}`
+  })
+  .when("/:page", {
+    templateUrl: (params) => `${params.page}.${templateFileExt}`
+  });
+};
+
+
+app.config([
+  "$routeProvider",
+  "$locationProvider",
+  routes
+]);
